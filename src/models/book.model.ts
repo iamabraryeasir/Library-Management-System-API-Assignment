4b8341a5a2656c00876e0cc755b819b5637b7931
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-06-19 22:38:24 +0600

    feat: create Book model with schema definition

---
+++ src/models/book.model.ts
@@ -0,0 +1,53 @@
+import { Schema, model } from 'mongoose';
+import { IBook } from '../interfaces/book.interface';
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Book title is required'],
+    },
+    author: {
+      type: String,
+      required: [true, 'Author name is required'],
+    },
+    genre: {
+      type: String,
+      enum: {
+        values: [
+          'FICTION',
+          'NON_FICTION',
+          'SCIENCE',
+          'HISTORY',
+          'BIOGRAPHY',
+          'FANTASY',
+        ],
+        message: '{VALUE} is not supported as a genre',
+      },
+      required: [true, 'Genre is required'],
+    },
+    isbn: {
+      type: String,
+      required: [true, 'ISBN number is required'],
+    },
+    description: {
+      type: String,
+    },
+    copies: {
+      type: Number,
+      required: [true, 'Number of copies must be provided'],
+      min: [0, "Copies can't be negative"],
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Book = model<IBook>('Book', bookSchema);
+export default Book;
